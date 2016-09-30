5c5ca0bcff1fbb2b026e0cb1e885188807bedda8
dallin-r-parker <dallin.r.parker@gmail.com>
2016-09-29 19:32:36 -0600

    practice

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
+    $(function(){
+        $('#anchor1').click(function(){
+            $('html, body').animate({
+                scrollTop: $('#part-two').onscroll().top},
+                2000
+            );
+        return false;
+        });
+    });
+
+    $(function(){
+        $('#anchor2').click(function(){
+            $('html, body').animate({
+                    scrollTop: $('#part-three').onscroll().top},
+                2000
+            );
+            return false;
+        });
+    });
 
 });
+
+
+
---
+++ myjQ.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(function (){
+        $('#anchor1').click(function () {
+            $('html, body').animate({
+                scrollTop: $('#part-two').offset().top},
+                2000
+            );
+        return false;
+        });
+    });
+
+});
+
+
+
